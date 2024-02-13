ca7de6d32d2d0e711705f1dfc5ccd9a6f7c83312
sergarbor <sergarbor@gmail.com>
2024-02-13 17:57:08 +0100

    Start capturing packets

--- src/cli.rs
+++ src/cli.rs
@@ -5,12 +5,3 @@ pub struct Cli {
     pub command: String,
     pub value: u8,
 }
-
-/*
-impl Cli {
-    pub fn new(command: String, value: Option<u8>) -> Self {
-        self.command = command;
-        self.value = value.unwrap_or(5); // default is 5
-    }
-}
-*/
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
 use clap::Parser;
 use cli::Cli;
+use pcap::Device;
 
+use chrono::prelude::*;
 pub mod cli;
+
+fn get_readable_time(time_stamp: String) -> String {
+    // Convert the timestamp string into an i64
+    let timestamp = time_stamp.parse::<i64>().unwrap();
+    // Create a NaiveDateTime from the timestamp
+    let naive = NaiveDateTime::from_timestamp(timestamp, 0);
+    // Create a normal DateTime from the NaiveDateTime
+    let datetime: DateTime<Utc> = DateTime::from_utc(naive, Utc);
+    // Format the datetime how you want
+    let newdate = datetime.format("%Y-%m-%d %H:%M:%S");
+    // Print the newly formatted date and time
+    return newdate.to_string();
+}
+
+fn capture_packets(n_packets: u8) {
+    let mut cap = Device::lookup().unwrap().unwrap().open().unwrap();
+    let mut cap_counter = 0;
+
+    while let Ok(packet) = cap.next_packet() {
+        if cap_counter >= n_packets {
+            break;
+        }
+        let capture_time = get_readable_time(packet.header.ts.tv_sec.to_string());
+        println!("- {} {:?}", capture_time, packet);
+        cap_counter += 1;
+    }
+}
 
 fn main() {
     let args = Cli::parse();
@@ -12,7 +41,11 @@ fn main() {
 
     println!("Command: {} Value {}", args.command, args.value);
 
-    if args.command == "check" {
-        println!("THIS IS THE EXPECTED COMMAND")
+    if args.command != "check" {
+        return;
     }
+
+    println!("THIS IS THE EXPECTED COMMAND!");
+
+    capture_packets(args.value);
 }
