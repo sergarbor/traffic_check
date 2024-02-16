2225f53a59b250d4f51d90e9d0233388fc0c585a
sergarbor <sergarbor@gmail.com>
2024-02-16 22:48:38 +0100

    add test

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -7,3 +7,42 @@ fn test_get_readable_time() {
     let date_str = traffic_check::get_readable_time(ts);
     assert_eq!(date_str, expected_time);
 }
+
+#[test]
+fn test_bytes_to_arp_operation() {
+    let operation_bytes = [0x00, 0x01];
+    let expected_time: String = String::from("ARP Request");
+
+    let date_str = traffic_check::bytes_to_arp_operation(&operation_bytes);
+    assert_eq!(date_str, expected_time);
+
+    let operation_bytes = [0x00, 0x02];
+    let expected_time: String = String::from("ARP Reply");
+
+    let date_str = traffic_check::bytes_to_arp_operation(&operation_bytes);
+    assert_eq!(date_str, expected_time);
+
+    let operation_bytes = [0x00, 0x03];
+    let expected_time: String = String::from("RARP Request");
+
+    let date_str = traffic_check::bytes_to_arp_operation(&operation_bytes);
+    assert_eq!(date_str, expected_time);
+
+    let operation_bytes = [0x00, 0x04];
+    let expected_time: String = String::from("RARP Reply");
+
+    let date_str = traffic_check::bytes_to_arp_operation(&operation_bytes);
+    assert_eq!(date_str, expected_time);
+
+    let operation_bytes = [0x00, 0x08];
+    let expected_time: String = String::from("InARP Request");
+
+    let date_str = traffic_check::bytes_to_arp_operation(&operation_bytes);
+    assert_eq!(date_str, expected_time);
+
+    let operation_bytes = [0x00, 0x09];
+    let expected_time: String = String::from("InARP Reply");
+
+    let date_str = traffic_check::bytes_to_arp_operation(&operation_bytes);
+    assert_eq!(date_str, expected_time);
+}
