a0d54f5015172f0b2d79f949a2c7b572d8f3cc17
sergarbor <sergarbor@gmail.com>
2024-02-18 21:14:29 +0100

    add test coverage

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -40,6 +40,12 @@ fn test_bytes_to_arp_operation() {
     let operation = traffic_check::bytes_to_arp_operation(&operation_bytes);
     assert_eq!(operation, expected_time);
 
+    let operation_bytes = [0xFF, 0xFF];
+    let expected_time: String = String::from("Unknown ARP Operation");
+
+    let operation = traffic_check::bytes_to_arp_operation(&operation_bytes);
+    assert_eq!(operation, expected_time);
+
     let operation_bytes = [0x00, 0x09];
     let expected_time: String = String::from("InARP Reply");
 
@@ -54,4 +60,49 @@ fn test_bytes_to_ip_address() {
 
     let ip_address = traffic_check::bytes_to_ip_address(&address_bytes);
     assert_eq!(ip_address, expected_time);
+}
+
+#[test]
+fn test_bytes_to_arp_protocol_type() {
+    let operation_bytes = [0x08, 0x00];
+    let expected_time: String = String::from("IPv4");
+
+    let operation = traffic_check::bytes_to_arp_protocol_type(&operation_bytes);
+    assert_eq!(operation, expected_time);
+
+    let operation_bytes = [0x08, 0x06];
+    let expected_time: String = String::from("ARP");
+
+    let operation = traffic_check::bytes_to_arp_protocol_type(&operation_bytes);
+    assert_eq!(operation, expected_time);
+
+    let operation_bytes = [0x86, 0xDD];
+    let expected_time: String = String::from("IPv6");
+
+    let operation = traffic_check::bytes_to_arp_protocol_type(&operation_bytes);
+    assert_eq!(operation, expected_time);
+
+    let operation_bytes = [0x80, 0x35];
+    let expected_time: String = String::from("RARP");
+
+    let operation = traffic_check::bytes_to_arp_protocol_type(&operation_bytes);
+    assert_eq!(operation, expected_time);
+
+    let operation_bytes = [0x80, 0x9B];
+    let expected_time: String = String::from("Appletalk");
+
+    let operation = traffic_check::bytes_to_arp_protocol_type(&operation_bytes);
+    assert_eq!(operation, expected_time);
+
+    let operation_bytes = [0xFF, 0xFF];
+    let expected_time: String = String::from("Unknown PTYPE");
+
+    let operation = traffic_check::bytes_to_arp_protocol_type(&operation_bytes);
+    assert_eq!(operation, expected_time);
+
+    let operation_bytes = [0x80, 0x9D];
+    let expected_time: String = String::from("Appletalk ARP");
+
+    let operation = traffic_check::bytes_to_arp_protocol_type(&operation_bytes);
+    assert_eq!(operation, expected_time);
 }
